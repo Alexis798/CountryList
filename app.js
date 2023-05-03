b6ba93d201200388058aa63e53ddcbaa5fb73ab2
Andres Alexis <andres.alexi07@gmail.com>
2023-05-03 15:12:53 -0400

    Version 1.0.0
    Con esta actualizacion ahora es posible consultar la lista de paises, estados y ciudades en el mundo.

--- app.js
+++ app.js
@@ -1,25 +1,118 @@
+const COUNTRIES = [];
+const STATES = [];
+const REGION = [];
+const CITIES = [];
+
+var headers = new Headers();
+headers.append("X-CSCAPI-KEY", "API_KEY");
+
+var requestOptions = {
+    method: 'GET',
+    headers: headers,
+    redirect: 'follow'
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     
     const selectDrop = document.querySelector('#countries');
     //const selectedDrop = document.getElementById('countries');
 
-
-    fetch('https://restcountries.com/v3.1/all').then(res => {
+    fetch('https://api.countrystatecity.in/v1/countries/', requestOptions).then(res => {
         return res.json();
     }).then(data => {
 
-        let output = "";
-
-        output += `<option value="NULL">-- Select Country --</option>`
-
-
         data.map(country => {
-            output += `<option value="${country.name.common}">${country.flag} ${country.name.common}</option>`
-        })
+            fetch(`https://api.countrystatecity.in/v1/countries/${country.iso2}`, requestOptions).then(res => {
+                return res.json();
+            }).then(result => {
+            
+                let output = "";
 
-        selectDrop.innerHTML = output
+                output += `<option value="NULL">-- Select Country --</option>`;
+                
+                COUNTRIES.push({id: result.id, name: result.name, emoji: result.emoji, iso: result.iso2})
+
+                COUNTRIES.sort((a, b) => {
+                    return a.id - b.id;
+                })
+
+                COUNTRIES.forEach(countryDetail => {
+                    output += `<option value="${countryDetail.name}">${countryDetail.emoji} ${countryDetail.name}</option>`
+                })
+
+                selectDrop.innerHTML = output
+            }).catch(err => {
+                console.log(err);
+            })
+        })
 
     }).catch(err => {
         console.log(err);
     })
+})
+
+document.addEventListener('change', (e) => {
+
+    
+    if(e.target.matches('#countries')) {
+        COUNTRIES.forEach(country => {
+            if(e.target.value == country.name) {
+
+                REGION.length = 0;
+                REGION.push(({idCountry: country.iso, idState: null}))
+
+                fetch(`https://api.countrystatecity.in/v1/countries/${country.iso}/states`, requestOptions).then(res => {
+                    return res.json();
+                }).then(result => {
+                    
+                    STATES.length = 0;
+
+                    result.map(state => {
+                        STATES.push(({id: state.id, name: state.name, iso: state.iso2}))
+                    })
+
+                    const selectDrop2 = document.querySelector('#states');
+
+                    let output = "";
+
+                    output += `<option value="NULL">-- Select State --</option>`;
+
+                    STATES.forEach(stateDetail => {
+                        output += `<option value="${stateDetail.name}">${stateDetail.name}</option>`
+                    })
+
+                    selectDrop2.innerHTML = output
+                })
+            }
+        })
+    }
+
+    if(e.target.matches('#states')) {
+        STATES.forEach(state => {
+            if(e.target.value == state.name) {
+                REGION[0].idState = state.iso
+                
+                REGION.forEach(country => {
+                    fetch(`https://api.countrystatecity.in/v1/countries/${country.idCountry}/states/${country.idState}/cities`, requestOptions).then(res => {
+                        return res.json();
+                    }).then(result => {
+                        
+                        const selectDrop3 = document.querySelector('#cities');
+
+                        let output = "";
+
+                        output += `<option value="NULL">-- Select City --</option>`;
+
+                        result.forEach(city => {
+                            output += `<option value="${city.name}">${city.name}</option>`
+                        })
+
+                        selectDrop3.innerHTML = output
+                    })
+                })
+            }
+        })
+    }
+
+    
 })
